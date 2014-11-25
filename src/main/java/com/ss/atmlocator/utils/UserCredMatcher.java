2881a2e4e0591b34cd471aba137cf904259fdbb1
jimmy-sv <s.vertepniy@gmail.com>
2014-11-25 17:18:26 +0200

    check cred rev

--- src/main/java/com/ss/atmlocator/utils/UserCredMatcher.java
+++ src/main/java/com/ss/atmlocator/utils/UserCredMatcher.java
@@ -1,53 +1,36 @@
 package com.ss.atmlocator.utils;
 
-
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+/**
+ *  Utility class for regexp checking login,password,email
+ */
 
 public class UserCredMatcher {
 
-    private String mailRegExp;
-    private String loginRegExp;
-    private String passwordRegExp;
-
     private Pattern emailPattern;
-    private Matcher emailMatcher;
-
     private Pattern loginPattern;
-    private Matcher loginMatcher;
-
     private Pattern passwordPattern;
-    private Matcher passwordMatcher;
 
+    /* Constructor with regexp as params*/
     public UserCredMatcher(String mailRegExp, String loginRegExp, String passwordRegExp) {
-
-        this.mailRegExp = mailRegExp;
-        this.loginRegExp = loginRegExp;
-        this.passwordRegExp = passwordRegExp;
-
         emailPattern = Pattern.compile(mailRegExp);
         loginPattern = Pattern.compile(loginRegExp);
         passwordPattern = Pattern.compile(passwordRegExp);
     }
 
+    /* check email Returns true if email id valid */
     public boolean validateEmail(final String email){
-
-        emailMatcher = emailPattern.matcher(email);
-        return emailMatcher.matches();
-
+        return emailPattern.matcher(email).matches();
     }
 
+    /* check login Returns true if login id valid*/
     public boolean validateLogin(final String login){
-
-        loginMatcher = loginPattern.matcher(login);
-        return loginMatcher.matches();
-
+        return loginPattern.matcher(login).matches();
     }
 
+    /* check login. Returns true if password id valid */
     public boolean validatePassword(final String password){
-
-        passwordMatcher = passwordPattern.matcher(password);
-        return passwordMatcher.matches();
-
+        return passwordPattern.matcher(password).matches();
     }
 }
