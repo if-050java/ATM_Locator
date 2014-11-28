aad8fefd62dcd2f066cf7bea365593756905d85e
unknown <student@ifclass19.training.local>
2014-11-28 19:02:49 +0200

    should fix

--- src/main/java/com/ss/atmlocator/utils/ValidateUserFields.java
+++ src/main/java/com/ss/atmlocator/utils/ValidateUserFields.java
@@ -3,12 +3,12 @@ package com.ss.atmlocator.utils;
 /**
  * Created by us8610 on 11/25/2014.
  */
-public class ErrorMessage {
+public class ValidateUserFields {
 
     private String fieldName;
     private String message;
 
-    public ErrorMessage(String fieldName, String message) {
+    public ValidateUserFields(String fieldName, String message) {
         this.fieldName = fieldName;
         this.message = message;
     }
