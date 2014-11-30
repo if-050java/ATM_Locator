b53dd08c3e97f62300d1cbb9481a87870b3373ba
Roman Vintonyak <roma.vintonyak@gmail.com>
2014-11-30 20:34:55 +0200

    password MD5 encoder

--- src/main/java/com/ss/atmlocator/validator/ImageValidator.java
+++ src/main/java/com/ss/atmlocator/validator/ImageValidator.java
@@ -1,7 +1,6 @@
 package com.ss.atmlocator.validator;
 
-import com.ss.atmlocator.service.ValidateUserCredCode;
-import com.ss.atmlocator.utils.UtilEnums;
+import com.ss.atmlocator.utils.Constants;
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
@@ -30,13 +29,13 @@ public class ImageValidator implements Validator {
         MultipartFile image = (MultipartFile) object;
         if (null == image) return;
         if (image.getSize() > MAX_FILE_SIZE) {
-            errors.rejectValue(UtilEnums.UserResponseField.AVATAR.toString(),
+            errors.rejectValue(Constants.USER_AVATAR,
                     messages.getMessage("file.size.limit", null, Locale.ENGLISH));
             return;
         } else if (!FilenameUtils.getExtension(image.getOriginalFilename()).equals("jpg") &&
                 !FilenameUtils.getExtension(image.getOriginalFilename()).equals("jpeg") &&
                 !FilenameUtils.getExtension(image.getOriginalFilename()).equals("png")) {
-            errors.rejectValue(UtilEnums.UserResponseField.AVATAR.toString(),
+            errors.rejectValue(Constants.USER_AVATAR,
                     messages.getMessage("file.extension", null, Locale.ENGLISH));
         }
     }
