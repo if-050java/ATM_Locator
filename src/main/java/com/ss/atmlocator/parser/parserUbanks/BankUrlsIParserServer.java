7ecb3b21593377737af605c14acf731422731ba7
Boichuk20 <konsyl77@gmail.com>
2014-12-11 18:23:19 +0200

    random deleted

--- src/main/java/com/ss/atmlocator/parser/parserUbanks/BankUrlsIParserServer.java
+++ src/main/java/com/ss/atmlocator/parser/parserUbanks/BankUrlsIParserServer.java
@@ -102,19 +102,7 @@ public class BankUrlsIParserServer implements IParser {
         return bankList;
     }
 
-    ////////////////////////////
-    public static int randInt(int min, int max) {
 
-        // NOTE: Usually this should be a field rather than a method
-        // variable so that it is not re-seeded every call.
-        Random rand = new Random();
-
-        // nextInt is normally exclusive of the top value,
-        // so add 1 to make it inclusive
-        int randomNum = rand.nextInt((max - min) + 1) + min;
-
-        return randomNum;
-    }
 
 }
 
