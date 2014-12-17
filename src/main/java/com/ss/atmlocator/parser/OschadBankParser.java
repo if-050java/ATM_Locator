646dc260b492bc53d326d3049f6d25f298f8bdba
olavin <olavin@ukr.net>
2014-12-17 02:10:59 +0200

    OschadBankParser modified

--- src/main/java/com/ss/atmlocator/parser/OschadBankParser.java
+++ src/main/java/com/ss/atmlocator/parser/OschadBankParser.java
@@ -20,10 +20,14 @@ import java.util.*;
 /**
  * Created by Olavin on 14.12.2014.
  */
-public class OschadBankParser implements IParsers {
+public class OschadBankParser implements IParser {
+    final String LAST_PAGE_SELECTOR = "font.text > a:nth-last-child(1)";
+    //final String CHARACTER_ENCODING = "UTF-8";
+    final String SET_FILTER_PARAM = "\u0428\u0443\u043A\u0430\u0442\u0438"; //"Шукати"
+    final int ADDRESS_COLUMN_BRANCH = 1;
+    final int ADDRESS_COLUMN_ATM = 0;
 
     private Map<String,String> parameters;
-    String siteEncoding = null;
 
     @Override
     public void setParameter(Map<String, String> parameters) {
@@ -31,13 +35,8 @@ public class OschadBankParser implements IParsers {
     }
 
     @Override
-    public List<Bank> parse() {
-        return null;
-    }
-
-    //@Override
-    public Set<AtmOffice> parseAtms(){
-        Set<AtmOffice> atms = new HashSet<>();
+    public List<AtmOffice> parse(){
+        List<AtmOffice> atms = new ArrayList<>();
         List<String> regions = getRegionList();
         for(int i=0; i < regions.size(); i++){
             System.out.println(i+". "+regions.get(i));
@@ -46,7 +45,7 @@ public class OschadBankParser implements IParsers {
         //String ifr2 = "Івано-Франківська область";
         //String ifr = "\u0406\u0432\u0430\u043D\u043E-\u0424\u0440\u0430\u043D\u043A\u0456\u0432\u0441\u044C\u043A\u0430 \u043E\u0431\u043B\u0430\u0441\u0442\u044C";
         //System.out.println("[ "+ifr + "=" + ifr2+" ]");
-        String testRegion = regions.get(0);
+        String testRegion = regions.get(2);
 
         String page = parameters.get("base_url")+parameters.get("branch_page");
         String selector = parameters.get("branch_selector");
@@ -55,87 +54,82 @@ public class OschadBankParser implements IParsers {
         return atms;
     }
 
-    private Elements getElementsList(String page, String selector) throws IOException {
-        return Jsoup.parse(new URL(page).openStream(), siteEncoding, page).select(selector);
+    private Elements getElementsList(Connection connection, String selector) throws IOException {
+        Connection.Response response = connection.execute();
+        return response.parse().select(selector);
     }
 
-    private String getBranchAddress(Element row){
-        final int addressColumn = 1;
-        String address = row.child(addressColumn).text();
-        System.out.println(address);
-        return address;
-    }
-
-    private List<String> parseTable(String page, String selector) throws IOException {
+    private List<String> parseTable(Connection connection, String selector) throws IOException {
         List<String> addressList = new ArrayList<>();
-        Elements rows = getElementsList(page, selector);
-        for (Element element : rows) {
-            addressList.add(getBranchAddress(element));
+        Connection.Response response = connection.execute();
+        Elements rows = response.parse().select(selector);//getElementsList(connection, selector);
+        for (Element row : rows) {
+            String address = row.child(ADDRESS_COLUMN_BRANCH).text();
+            addressList.add(address);
+            System.out.println(address);
         }
         return addressList;
     }
 
-    String createRequestUrl(String pageUrl, String regionName, int pageNum) {
-        String requestUrl = null;
-        try {
-            String regionParam = "arrFilter_pf%5BRegionName%5D="+URLEncoder.encode(regionName, "UTF-8");
-            String otherParams = "&ib=atms_ua";
-            String searchParam = "&set_filter=" + URLEncoder.encode("\u0428\u0443\u043A\u0430\u0442\u0438", "UTF-8"); //"Шукати"
-            String pageNumParam = (pageNum > 1) ? "&PAGEN_1="+pageNum : "";
-            requestUrl = pageUrl + "?" + regionParam + otherParams + searchParam + pageNumParam;
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return requestUrl;
+    Connection createJsoupConnection(String pageUrl, String regionName, int pageNum) {
+        return Jsoup.connect(pageUrl)
+                .data("arrFilter_pf[RegionName]",regionName)
+                .data("ib","atms_ua")
+                .data("set_filter",SET_FILTER_PARAM)
+                .data("PAGEN_1",(pageNum > 1) ? String.valueOf(pageNum) : "");
     }
 
     private List<String> parseRegion(String pageUrl, String regionName, String selector){
         List<String> addressList = new ArrayList<>();
         try {
-
-            int pageCount = getPageCount(createRequestUrl(pageUrl, regionName, 1));
-            for(int i = 1; i <= pageCount; i++){
-                System.out.println("Page "+i);
-                addressList.addAll(parseTable(createRequestUrl(pageUrl, regionName, i), selector));
+            int page = 1;
+            int pageCount = 1;
+            do {
+                if(page == 1){
+                    pageCount = getPageCount(createJsoupConnection(pageUrl, regionName, page));
+                }
+                System.out.println("Page "+page);
+                addressList.addAll(parseTable(createJsoupConnection(pageUrl, regionName, page), selector));
                 System.out.println();
-            }
+                page++;
+            } while(page <= pageCount);
         } catch (IOException e) {
             e.printStackTrace();
+            //TODO: catch exception
         }
         return addressList;
-
     }
 
-    private int getPageCount(String page) throws IOException {
-        final String pageCountSelector = "font.text > a:nth-last-child(1)";
-        Elements elems = Jsoup.parse(new URL(page).openStream(), siteEncoding, page).select(pageCountSelector);
+    private int getPageCount(Connection connection) throws IOException {
+        Connection.Response response = connection.execute();
+        Elements elems = response.parse().select(LAST_PAGE_SELECTOR);
+        if(elems.size() == 0){
+            throw new IOException();
+        }
         String lastPageUrl = elems.get(0).attr("href");
         int index = lastPageUrl.lastIndexOf('=');
-        int lastPageNum = Integer.parseInt(lastPageUrl.substring(index+1));
+        int lastPageNum = Integer.parseInt(lastPageUrl.substring(index + 1));
         return (lastPageNum > 0 ? lastPageNum : 1);
-
     }
 
     private static List<String> getRegionList(){
         Set<String> regions = new HashSet<>();
+        ArrayList<String> regionList = new ArrayList<>(regions.size());
         try {
             Connection.Response response = Jsoup.connect("http://www.oschadnybank.com/handlers/region1.php")
                 .data("contentType", "application/json; charset=utf-8", "ib", "atms_ua", "p", "1", "s", "15")
                 .method(Connection.Method.GET)
                 .header("Accept", "application/json")
                 .header("Host", "www.oschadnybank.com")
-                .header("Referer", "http://www.oschadnybank.com/ua/branches_atms/atms/")
-                .header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0")
+                .referrer("http://www.oschadnybank.com/ua/branches_atms/atms/")
                 .header("X-Requested-With", "XMLHttpRequest")
+                .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0")
                 .ignoreContentType(true)
                 .execute();
             Document document = Jsoup.parse(response.parse().outerHtml());
-            //System.out.println(document.text());
-            //String JsonString = ;
 
             JSONObject JsonObj = (JSONObject)JSONValue.parse(document.text());
             JSONArray jsonArray = (JSONArray)JsonObj.get("results");
-            //System.out.println(jsonArray);
             JSONObject JsonItem;
             String regionStr;
             for(int i=0; i<jsonArray.size(); i++){
@@ -143,13 +137,13 @@ public class OschadBankParser implements IParsers {
                 regionStr = (String) JsonItem.get("id");
                 regions.add(regionStr.trim());
             }
+            regionList = new ArrayList<>(regions.size());
+            regionList.addAll(regions);
+            Collections.sort(regionList);
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-        ArrayList<String> regionList = new ArrayList<>(regions.size());
-        regionList.addAll(regions);
-        Collections.sort(regionList);
         return regionList;
 
     }
@@ -171,7 +165,7 @@ public class OschadBankParser implements IParsers {
 
         OschadBankParser bankParser = new OschadBankParser();
         bankParser.setParameter(parameters);
-        bankParser.parseAtms();
+        bankParser.parse();
 
     }
 
