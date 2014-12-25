35bdabab83eeaeea00e4ca1a6b87cc55d08f6f82
MaksimToll <tolstik.maksim@yandex.ua>
2014-12-25 17:05:03 +0200

    aa

--- dump141222.sql
+++ dump141222.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.6.21, for Win64 (x86_64)
+﻿-- MySQL dump 10.13  Distrib 5.6.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: atmlocator
 -- ------------------------------------------------------
