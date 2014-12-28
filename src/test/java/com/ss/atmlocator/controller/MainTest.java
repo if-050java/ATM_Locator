9b8d1a84ec38b6e8d68a4840e56f23897a0234a1
olavin <olavin@ukr.net>
2014-12-28 14:25:16 +0200

    Tests modified

--- src/test/java/com/ss/atmlocator/controller/MainTest.java
+++ src/test/java/com/ss/atmlocator/controller/MainTest.java
@@ -10,9 +10,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.security.Principal;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 /**
@@ -38,5 +39,22 @@ public class MainTest {
         mockMvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("main"));
+    }
+
+    @Test
+    public void adminBanks() throws Exception {
+        Principal user = new Principal() {
+            @Override
+            public String getName() {
+                return "testUser";
+            }
+        };
+
+        mockMvc.perform(get("/adminBanks").principal(user)).andExpect(status().isOk())
+            .andExpect(view().name("adminBanks"))
+            .andExpect(model().attribute("userName", "testUser"))
+            .andExpect(model().attribute("active", "adminBanks"))
+            .andExpect(model().attributeExists("networks"));
+
     }
 }
