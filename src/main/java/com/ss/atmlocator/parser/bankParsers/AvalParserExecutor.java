4775590ce7eea6bf85250a10819ad9971aaefb7c
MaksimToll <tolstik.maksim@yandex.ua>
2015-01-13 20:57:58 +0200

    ...

--- src/main/java/com/ss/atmlocator/parser/bankParsers/AvalParserExecutor.java
+++ src/main/java/com/ss/atmlocator/parser/bankParsers/AvalParserExecutor.java
@@ -18,9 +18,4 @@ public class AvalParserExecutor extends ParserExecutor {
         logger.trace("[PARSER EXECUTOR] parser set successfully");
     }
 
-
-//        parameters.put("bankUrl", "http://api.finlocator.com/features/?filters=&theme=aval&section=atm&lat=48.922633&lon=24.71111700000006&lang=ua&filters_atm=&filters_branch=&filters_all=,&_=1418811510013");
-//        parameters.put("officeUrl", "http://api.finlocator.com/features/?filters=&theme=aval&section=branch&city=29819&lat=48.922633&lon=24.71111700000006&lang=ua&filters_atm=&filters_branch=,,&filters_all=,&_=1418814350710");
-//        parameters.put("bankId", "30");
-
 }
